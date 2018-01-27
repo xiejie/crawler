d7e382ea2c7976788d64bb2397e3a605dcd92c0b
xiejie <xiejie2104@qq.com>
2018-01-27 13:43:50 +0800

    test

--- tests/unit/KeywordTest.php
+++ tests/unit/KeywordTest.php
@@ -6,14 +6,12 @@ class KeywordTest extends TestCase
     public function testSearch()
     {
         $driver = Crawler\ChromeDriver::getInstance(['--proxy-server=socks5://127.0.0.1:1080']);
-        $word = [
-            'url' => 'www.google.com',
-            'keyword' => 'google'
-        ];
+        $data = (new Crawler\Google)->searchKeyword($driver, 'google', 'www.google.com');
 
-        (new Crawler\Google)->searchKeyword($driver, $word['keyword'], $word['url']);
-
-//         $this->assertTrue(true);
+        $this->assertEquals(
+                ['keyword' => 'google' , 'page' => 1, 'rank' => 1],
+                $data
+            );
     }
 
 }
